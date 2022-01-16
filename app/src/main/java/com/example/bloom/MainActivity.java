5fe8207cb6bc363f74a3983499cd3a2594390135
Eric Wong <erichwlwong@gmail.com>
2022-01-15 18:47:10 -0800

    test commit

--- app/src/main/java/com/example/bloom/MainActivity.java
+++ app/src/main/java/com/example/bloom/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.bloom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+
+        Button button = (Button) findViewById(R.id.button_send);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
     }
 }
