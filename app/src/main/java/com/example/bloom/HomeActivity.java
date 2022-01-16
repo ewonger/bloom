a2283f8688bef48f61ecd8bf93698111169ebe41
ewonger <erichwlwong@gmail.com>
2022-01-16 07:43:53 -0800

    merge

--- app/src/main/java/com/example/bloom/HomeActivity.java
+++ app/src/main/java/com/example/bloom/HomeActivity.java
@@ -7,17 +7,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import veryfi.*;
 
 public class HomeActivity extends AppCompatActivity {
 
     ImageView imageView;
     ActivityResultLauncher<Intent> mGetContent;
+    File f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
@@ -33,13 +45,26 @@ public class HomeActivity extends AppCompatActivity {
         active_user_tv.setText(active_user);
 
         imageView = findViewById(R.id.imageView);
-        mGetContent = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
-            @Override
-            public void onActivityResult(ActivityResult result) {
-                Intent data = result.getData();
-                Bitmap image = (Bitmap) data.getExtras().get("data");
-                imageView.setImageBitmap(image);
-            }
+        mGetContent = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            Intent data = result.getData();
+            assert data != null;
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            saveFile(image);
+
+            String clientId = BuildConfig.CLIENT_ID;
+            String clientSecret = BuildConfig.CLIENT_SECRET;
+            String username = BuildConfig.USERNAME;
+            String apiKey = BuildConfig.APIKEY;
+
+            Client client = VeryfiClientFactory.createClient( clientId, clientSecret, username, apiKey);
+            System.out.println(client);
+//            VeryfiAPI v = new VeryfiAPI();
+//            System.out.println(v.verifyUser());
+
+
+
+//            imageView.setImageBitmap(image);
+            setContentView(R.layout.activity_receipt);
         });
     }
 
@@ -53,4 +78,35 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(i);
     }
 
+    public void saveFile(Bitmap bitmap) {
+        String timeStamp = new SimpleDateFormat("yyyy-MM-dd HHmmss").format(new Date());
+        FileOutputStream fOut = null;
+
+        f = new File(Environment.getExternalStorageDirectory()
+                .toString() + "/" + timeStamp + ".png");
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            System.out.println("err" + e);
+        }
+        try {
+            fOut = new FileOutputStream(f);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+
+        try {
+            assert fOut != null;
+            fOut.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ app/src/main/java/com/example/bloom/VeryfiAPI.java
@@ -0,0 +1,24 @@
+package com.example.bloom;
+
+import java.util.Arrays;
+import java.util.List;
+
+import veryfi.*;
+
+public class VeryfiAPI {
+    public VeryfiAPI() {
+    }
+
+    public String verifyUser() {
+        String clientId = BuildConfig.CLIENT_ID;
+        String clientSecret = BuildConfig.CLIENT_SECRET;
+        String username = BuildConfig.USERNAME;
+        String apiKey = BuildConfig.APIKEY;
+
+        Client client = VeryfiClientFactory.createClient( clientId, clientSecret, username, apiKey);
+        List<String> categories = Arrays.asList("Advertising & Marketing", "Automotive");
+        String jsonResponse = client.processDocument("example1.jpg", categories, false, null);
+        return jsonResponse;
+    }
+}
+
