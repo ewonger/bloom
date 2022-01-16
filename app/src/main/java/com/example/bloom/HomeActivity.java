82f5cdb8967f1f922b4c576af8d9914fb0b71385
ewonger <erichwlwong@gmail.com>
2022-01-16 03:09:01 -0800

    added save photos

--- app/src/main/java/com/example/bloom/HomeActivity.java
+++ app/src/main/java/com/example/bloom/HomeActivity.java
@@ -9,9 +9,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -20,6 +28,7 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
@@ -30,13 +39,12 @@ public class HomeActivity extends AppCompatActivity {
         active_user_tv.setText(active_user);
 
         imageView = findViewById(R.id.imageView);
-        mGetContent = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
-            @Override
-            public void onActivityResult(ActivityResult result) {
-                Intent data = result.getData();
-                Bitmap image = (Bitmap) data.getExtras().get("data");
-                imageView.setImageBitmap(image);
-            }
+        mGetContent = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            Intent data = result.getData();
+            assert data != null;
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            saveFile(image);
+            imageView.setImageBitmap(image);
         });
     }
 
@@ -50,4 +58,36 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(i);
     }
 
+    public void saveFile(Bitmap bitmap) {
+        String timeStamp = new SimpleDateFormat("yyyy-MM-dd HHmmss").format(new Date());
+        FileOutputStream fOut = null;
+
+        File f = new File(Environment.getExternalStorageDirectory()
+                .toString() + "/" + timeStamp + ".png");
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            System.out.println("err" + e);
+        }
+        try {
+            fOut = new FileOutputStream(f);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+
+        try {
+            assert fOut != null;
+            fOut.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
