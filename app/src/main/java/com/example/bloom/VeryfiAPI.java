a050962b21d6541098f88bc79223b6074ce1c485
ewonger <erichwlwong@gmail.com>
2022-01-16 08:21:00 -0800

    fix error

--- app/src/main/java/com/example/bloom/VeryfiAPI.java
+++ app/src/main/java/com/example/bloom/VeryfiAPI.java
@@ -9,16 +9,16 @@ public class VeryfiAPI {
     public VeryfiAPI() {
     }
 
-    public String verifyUser() {
+    public void verifyUser() {
         String clientId = BuildConfig.CLIENT_ID;
         String clientSecret = BuildConfig.CLIENT_SECRET;
         String username = BuildConfig.USERNAME;
         String apiKey = BuildConfig.APIKEY;
 
-        Client client = VeryfiClientFactory.createClient( clientId, clientSecret, username, apiKey);
-        List<String> categories = Arrays.asList("Advertising & Marketing", "Automotive");
-        String jsonResponse = client.processDocument("example1.jpg", categories, false, null);
-        return jsonResponse;
+//        Client client = VeryfiClientFactory.createClient( clientId, clientSecret, username, apiKey);
+//        List<String> categories = Arrays.asList("Advertising & Marketing", "Automotive");
+//        String jsonResponse = client.processDocument("example1.jpg", categories, false, null);
+//        return jsonResponse;
     }
 }
 
